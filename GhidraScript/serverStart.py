a8d1a6face0b5bfcf9bd412ee74097609feb86fd
Keith <redundently1234@gmail.com>
2023-01-16 14:19:35 -0500

    [FEATURE] server ipc script runs in separate thread (in background). improved goto ipc cmd.

--- GhidraScript/serverStart.py
+++ GhidraScript/serverStart.py
@@ -2,6 +2,9 @@ from multiprocessing import shared_memory
 from enum import Enum
 import struct
 import time
+import threading
+from ghidra.app.script import GhidraState
+from ghidra.app.script import GhidraScript
 
 class Command(Enum):
     SHUTDOWN = b'\x01'
@@ -13,19 +16,33 @@ class Packet:
         self.message_type = message_type
         self.payload = payload
 
-class PyhidraEmuClient:
+class PyhidraEmuServer(threading.Thread):
     def __init__(self):
+        super(PyhidraEmuServer, self).__init__()
+        self.daemon = True
         self.server_shm = None
-        if self.init_shared_memory():
-            self.command_handler()
-        else:
-            self.shutdown()
+        self.init_shared_memory()
+        self.last_goto_addr = None
+
+
+        # self.shutdown_via_ipc()
+
+    def run(self):
+        print("[PYHIDRA-EMU] Server thread is running!")
+        self.is_serving = True
+        self.command_handler()
+        print("[PYHIDRA-EMU] Server thread is exiting!")
+
+    def __del__(self):
+        self.do_shutdown()
+        self.shutdown()
 
     def init_shared_memory(self):
         try:
             self.server_shm = shared_memory.SharedMemory(create=True, size=0x100, name="pyhidraEmu")
         except:
-            return False
+            print("[PYHIDRAEMU-SERVER] Shared memory still exists for IPC, reusing.")
+            self.server_shm = shared_memory.SharedMemory(name='pyhidraEmu')
 
         print("[PYHIDRAEMU-SERVER] Initialized shared memory for IPC.")
         return True
@@ -56,9 +73,15 @@ class PyhidraEmuClient:
                 self.do_shutdown()
                 break
     def goto_address(self, address: int):
-        if toAddr(address) != currentAddress:
+        # gs = getState()
+        addrObj = toAddr(address)
+        print(f"currentAddress: {hex(int(currentAddress.toString(), 16))} , addrObj: {hex(int(addrObj.toString(), 16))}")
+        if self.last_goto_addr != address:
+            self.last_goto_addr = address
             print(f'Going to address {hex(address)}')
-            goTo(toAddr(address))
+            goTo(addrObj)
+            # currentAddress doesn't update when using this....
+            # gs.setCurrentAddress(addrObj)
 
 
     def do_shutdown(self):
@@ -66,7 +89,7 @@ class PyhidraEmuClient:
         self.server_shm.unlink()
         print('[PYHIDRAEMU-SERVER] Closed and unlinked server shared memory')
 
-    def shutdown(self):
+    def shutdown_via_ipc(self):
         print("[PYHIDRAEMU-SERVER] Detected server shared memory, sending server shutdown packet")
         try:
             server_shm = shared_memory.SharedMemory(name="pyhidraEmu")
@@ -74,14 +97,13 @@ class PyhidraEmuClient:
             print("[PYHIDRAEMU-SERVER] Unable to open shared memory \'pyhidraEmu\'")
             return
 
-
-
         packet = Packet(Command.SHUTDOWN, b'0')
         raw_packet = struct.pack('<bi', packet.message_type, len(packet.payload)) + packet.payload
         server_shm.buf = raw_packet
 
 
 if __name__ == '__main__':
-    PyhidraEmuClient()
+    pyEmu_serv = PyhidraEmuServer()
+    pyEmu_serv.start()
 
 
